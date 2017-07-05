f666516ed8ae96f23234ddfcf7c2d445decfc506
Mike Joyce <mikejoyce19@gmail.com>
2017-07-04 21:48:27 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const webApiEndpoint = '/api/query-stats'
+
+module.exports = function(context) {
+  //on a timer...call out to twitter api to find mentions
+
+  //if any, extract query out of tweet
+
+  //run compiler over query, get statQuery object
+
+  //post statQuery object to api
+
+  //get response from api, convert into tweet response
+
+  //what is tweet response??? text or image?
+}
